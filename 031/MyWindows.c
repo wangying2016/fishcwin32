781ba7abe43c522d422d5ba0c63ffb31e891384a
wangying <867504146@qq.com>
2017-04-09 22:34:17 +0800

    提交31课代码

---
+++ 031/MyWindows.c
@@ -0,0 +1,103 @@
+/* -------------------------------------------------------------------
+                    MyWindows.c -- 基本窗口模型  
+				《Windows 程序设计（SDK）》视频教程                    
+--------------------------------------------------------------------*/
+
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void DrawRectangle(HWND hwnd);
+
+int cxClient, cyClient;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+	static TCHAR szAppName[] = TEXT("MyWindows");
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS wndclass;
+
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = szAppName;
+
+	if (!RegisterClass(&wndclass))
+	{
+		MessageBox(NULL, TEXT("这个程序需要在 Windows NT 才能执行！"), szAppName, MB_ICONERROR);
+		return 0;
+	}
+
+	hwnd = CreateWindow(szAppName, 
+		TEXT("鱼C工作室"), 
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT,
+		NULL, 
+		NULL, 
+		hInstance, 
+		NULL);
+	
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	while (TRUE)
+	{
+		if (PeekMessage(&msg, hwnd, NULL, NULL, PM_REMOVE)) {
+			if (msg.message == WM_QUIT) {
+				break;
+			}
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		} else {
+			DrawRectangle(hwnd);
+		}
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT rect;
+
+	switch (message)
+	{
+	case WM_SIZE:
+		cxClient = LOWORD(lParam);
+		cyClient = HIWORD(lParam);
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+void DrawRectangle(HWND hwnd)
+{
+	HBRUSH hBrush;
+	HDC hdc;
+	RECT rect;
+
+	SetRect(&rect, rand() % cxClient, rand() % cyClient, rand() % cxClient, rand() % cyClient);
+	hBrush = CreateSolidBrush(RGB(rand() % 256, rand() % 256, rand() % 256));
+
+	hdc = GetDC(hwnd);
+	FillRect(hdc, &rect, hBrush);
+
+	DeleteObject(hBrush);
+	ReleaseDC(hwnd, hdc);
+}
