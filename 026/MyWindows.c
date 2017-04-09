b3ee03a206d18197bf334a2d281894d55adfb56f
wangying <867504146@qq.com>
2017-04-09 14:37:57 +0800

    提交26课代码修复

--- 026/MyWindows.c
+++ 026/MyWindows.c
@@ -74,21 +74,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_PAINT:
 		hdc = BeginPaint(hwnd, &ps);
 		
+		// 绘制辅助线
 		MoveToEx(hdc, 0, cyClient / 2, NULL);
 		LineTo(hdc, cxClient, cyClient / 2);
 		MoveToEx(hdc, cxClient / 2, 0, NULL);
 		LineTo(hdc, cxClient / 2, cyClient);
 
-		iMapMode = GetMapMode(hdc);
-
+		// 设置当前映射模式为 MM_LOMETRIC，即每个逻辑单位映射为0.1毫米，x轴向右，y轴向上
+		// 这样将窗口的映射模式类同于数学中的坐标系模式
 		SetMapMode(hdc, MM_LOMETRIC);
-		SetViewportOrgEx(hdc, 0, cyClient / 2, NULL);
-		
+		// 指定映射到窗口原点(0, 0)的设备原点的坐标，即指定哪个设备原点将映射到逻辑原点(0, 0)
+		// 这里是指定的窗口的最左侧居中的位置为我们的逻辑原点(0, 0)在实际的设备上的映射
+		// 这里指定的是窗口中心为坐标系原点位置，符合数学习惯
+		SetViewportOrgEx(hdc, cxClient / 2, cyClient / 2, NULL);
+		// 这里从最左边居中的位置开始绘制正弦图像
 		MoveToEx(hdc, -cxClient / 2, 0, NULL);
 		for (double x = -cxClient / 2; x < cxClient / 2; x += 0.01) {
+			// 这里的两个坐标都进行了一部分的放大，其中x += 0.01 是为了细分曲线
 			LineTo(hdc, x * 200, sin(x) * 300);
 		}
-
 
 		EndPaint(hwnd, &ps);
 		return 0;
