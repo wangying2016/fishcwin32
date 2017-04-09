9beb4cfd8ab1b9d8264e062d523f43edfa510893
wangying <867504146@qq.com>
2017-04-09 12:05:12 +0800

    提交24课哆啦A梦代码

--- 024/MyWindows.c
+++ 024/MyWindows.c
@@ -32,7 +32,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 	}
 
 	hwnd = CreateWindow(szAppName, 
-		TEXT("鱼C工作室"), 
+		TEXT("哆啦A梦"), 
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, 
 		CW_USEDEFAULT, 
@@ -60,7 +60,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 	HDC hdc;
 	PAINTSTRUCT ps;
 	HPEN hPen, hOldPen;
-	HBRUSH hBlueBrush, hRedBrush, hOldBrush;
+	HBRUSH hBlueBrush, hRedBrush, hOldBrush, hYellowBrush;
+	POINT apt[128];
 	static int cxClient, cyClient;
 
 	switch (message)
@@ -83,7 +84,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		SelectObject(hdc, hOldPen);
 
 		// 头（直径240）
-		hBlueBrush = CreateSolidBrush(RGB(0, 159, 132));
+		hBlueBrush = CreateSolidBrush(RGB(0, 159, 232));
 		hOldBrush = SelectObject(hdc, hBlueBrush);
 		Ellipse(hdc, cxClient / 2 - 120, cyClient / 2 - 200, cxClient / 2 + 120, cyClient / 2 + 40);
 		SelectObject(hdc, hOldBrush);
@@ -128,6 +129,87 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		LineTo(hdc, cxClient / 2 + 20, cyClient / 2 - 85);
 		MoveToEx(hdc, cxClient / 2 + 70, cyClient / 2 - 55, NULL);
 		LineTo(hdc, cxClient / 2 + 20, cyClient / 2 - 70);
+
+		// 身体
+		hOldBrush = SelectObject(hdc, hBlueBrush);
+		Rectangle(hdc, cxClient / 2 - 90, cyClient / 2, cxClient / 2 + 90, cyClient / 2 + 150);
+		SelectObject(hdc, hOldBrush);
+
+		// 肚子
+		Ellipse(hdc, cxClient / 2 - 70, cyClient / 2 - 20, cxClient / 2 + 70, cyClient / 2 + 120);
+		hPen = CreatePen(PS_SOLID, 2, RGB(255, 255, 255));
+		hOldPen = SelectObject(hdc, hPen);
+		Arc(hdc, cxClient / 2 - 70, cyClient / 2 - 20, cxClient / 2 + 70, cyClient / 2 + 120, \
+			cxClient / 2 + 60, cyClient / 2 - 10, cxClient / 2 - 60, cyClient / 2 - 10);
+		SelectObject(hdc, hOldPen);
+
+		// 项圈
+		hOldBrush = SelectObject(hdc, hRedBrush);
+		RoundRect(hdc, cxClient / 2 - 95, cyClient / 2 - 5, cxClient / 2 + 95, cyClient / 2 + 10, 20, 20);
+		SelectObject(hdc, hOldBrush);
+
+		// 铃铛
+		hYellowBrush = CreateSolidBrush(RGB(255, 255, 0));
+		hOldBrush = SelectObject(hdc, hYellowBrush);
+		Ellipse(hdc, cxClient / 2 - 15, cyClient / 2, cxClient / 2 + 15, cyClient / 2 + 30);
+		SelectObject(hdc, hOldBrush);
+		RoundRect(hdc, cxClient / 2 - 15, cyClient / 2 + 10, cxClient / 2 + 15, cyClient / 2 + 15, 2, 2);
+		SelectObject(hdc, hRedBrush);
+		Ellipse(hdc, cxClient / 2 - 4, cyClient / 2 + 18, cxClient / 2 + 4, cyClient / 2 + 26);
+		MoveToEx(hdc, cxClient / 2, cyClient / 2 + 26, NULL);
+		LineTo(hdc, cxClient / 2, cyClient / 2 + 30);
+		SelectObject(hdc, hOldBrush);
+
+		// 口袋
+		Pie(hdc, cxClient / 2 - 50, cyClient / 2, cxClient / 2 + 50, cyClient / 2 + 100, \
+			cxClient / 2 - 50, cyClient / 2 + 50, cxClient / 2 + 50, cyClient / 2 + 50);
+
+		// 脚
+		Pie(hdc, cxClient / 2 - 20, cyClient / 2 + 130, cxClient / 2 + 20, cyClient / 2 + 170, \
+			cxClient / 2 + 20, cyClient / 2 + 150, cxClient / 2 - 20, cyClient / 2 + 150);
+		hPen = CreatePen(PS_SOLID, 2, RGB(255, 255, 255));
+		hOldPen = SelectObject(hdc, hPen);
+		MoveToEx(hdc, cxClient / 2 - 20, cyClient / 2 + 150, NULL);
+		LineTo(hdc, cxClient / 2 + 20, cyClient / 2 + 150);
+		SelectObject(hdc, hOldPen);
+
+		Ellipse(hdc, cxClient / 2 - 110, cyClient / 2 + 130, cxClient / 2 - 10, cyClient / 2 + 170);
+		Ellipse(hdc, cxClient / 2 + 110, cyClient / 2 + 130, cxClient / 2 + 10, cyClient / 2 + 170);
+
+		// 手
+		hOldBrush = SelectObject(hdc, hBlueBrush);
+		apt[0].x = cxClient / 2 - 90;
+		apt[0].y = cyClient / 2 + 10;
+		apt[1].x = cxClient / 2 - 130;
+		apt[1].y = cyClient / 2 + 50;
+		apt[2].x = cxClient / 2 - 110;
+		apt[2].y = cyClient / 2 + 70;
+		apt[3].x = cxClient / 2 - 90;
+		apt[3].y = cyClient / 2 + 60;
+		Polygon(hdc, apt, 4);
+		SelectObject(hdc, hOldBrush);
+		Ellipse(hdc, cxClient / 2 - 150, cyClient / 2 + 46, cxClient / 2 - 110, cyClient / 2 + 86);
+
+		hOldBrush = SelectObject(hdc, hBlueBrush);
+		apt[0].x = cxClient / 2 + 90;
+		apt[0].y = cyClient / 2 + 10;
+		apt[1].x = cxClient / 2 + 130;
+		apt[1].y = cyClient / 2 + 50;
+		apt[2].x = cxClient / 2 + 110;
+		apt[2].y = cyClient / 2 + 70;
+		apt[3].x = cxClient / 2 + 90;
+		apt[3].y = cyClient / 2 + 60;
+		Polygon(hdc, apt, 4);
+		SelectObject(hdc, hOldBrush);
+		Ellipse(hdc, cxClient / 2 + 150, cyClient / 2 + 46, cxClient / 2 + 110, cyClient / 2 + 86);
+
+		hPen = CreatePen(PS_SOLID, 2, RGB(0, 159, 232));
+		hOldPen = SelectObject(hdc, hPen);
+		MoveToEx(hdc, cxClient / 2 - 90, cyClient / 2 + 10, NULL);
+		LineTo(hdc, cxClient / 2 - 90, cyClient / 2 + 50);
+		MoveToEx(hdc, cxClient / 2 + 90, cyClient / 2 + 10, NULL);
+		LineTo(hdc, cxClient / 2 + 90, cyClient / 2 + 50);
+		SelectObject(hdc, hOldPen);
 
 		EndPaint(hwnd, &ps);
 		return 0;
