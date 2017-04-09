d9a1dbc96dca0c664890ef38284621e997a98a76
wangying <867504146@qq.com>
2017-04-09 20:06:04 +0800

    提交28课代码，关于MM_ISOTROPIC映射模式和MM_ANISOTROPIC映射模式的区别

---
+++ 028/MyWindows.c
@@ -0,0 +1,103 @@
+/* -------------------------------------------------------------------
+                    MyWindows.c -- 基本窗口模型  
+				《Windows 程序设计（SDK）》视频教程                    
+--------------------------------------------------------------------*/
+
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+	static TCHAR szAppName[] = TEXT("MyWindows");
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS wndclass;
+
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = szAppName;
+
+	if (!RegisterClass(&wndclass))
+	{
+		MessageBox(NULL, TEXT("这个程序需要在 Windows NT 才能执行！"), szAppName, MB_ICONERROR);
+		return 0;
+	}
+
+	hwnd = CreateWindow(szAppName, 
+		TEXT("鱼C工作室"), 
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT,
+		NULL, 
+		NULL, 
+		hInstance, 
+		NULL);
+	
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+
+	int iMapMode;
+	static int cxClient, cyClient;
+
+	switch (message)
+	{
+	case WM_SIZE:
+		cxClient = LOWORD(lParam);
+		cyClient = HIWORD(lParam);
+		return 0;
+
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+
+		iMapMode = GetMapMode(hdc);
+		SetMapMode(hdc, MM_ISOTROPIC);
+
+		// 设置窗口的水平范围和垂直范围均为100,
+		SetWindowExtEx(hdc, 100, 100, NULL);
+
+		// 刚刚窗口的100逻辑范围相当于视口的1/2宽度和高度，
+		// 也就是说，整个客户区应该是长和宽都是200逻辑单位
+		SetViewportExtEx(hdc, cxClient / 2, cyClient / 2, NULL);
+
+		// 设置窗口的原点在视口的中间位置显示
+		SetViewportOrgEx(hdc, cxClient / 2, cyClient / 2, NULL);
+
+		// 在客户区的中间画一个半径是80的圆
+		Ellipse(hdc, -80, -80, 80, 80);
+
+		SetMapMode(hdc, iMapMode);
+
+		EndPaint(hwnd, &ps);
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, message, wParam, lParam);
+}
